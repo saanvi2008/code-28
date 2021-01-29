169be83cb73c1c8e111fe5a764a2454588348746
saanvi2008 <75122796+saanvi2008@users.noreply.github.com>
2021-01-29 16:33:04 +0530

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,29 @@
+class stone{
+    constructor(x,y,r){
+        var options = {
+            restitution : 0,
+            isStatic : false,
+            friction : 1,
+            density : 1.2
+        }
+
+        this.image = loadImage("stone.png");
+
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.stone = Bodies.circle(this.x,this.y,this.r/2);
+        World.add(world,this.stone);
+
+    }
+    display(){
+        var pos = this.stone.position;
+
+        push();
+        translate(pos.x,pos.y);
+        imageMode(CENTER);
+        image(this.image,0,0,this.r,this.r);
+        pop();
+    
+    }
+}
